62b744ce1d5bb6efffddf7b30621c004fee069fa
TheLtWilson <rileywilsonpersonal@gmail.com>
2024-03-19 09:51:01 -0400

    Update branding to remove RCWDEV

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'RCWDEV';
+export const SITE_TITLE = 'Riley Wilson';
 export const SITE_DESCRIPTION = 'An incredibly part time video editor and web developer.';
